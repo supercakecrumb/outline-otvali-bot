ab1185df1fe614619c875c0461ae946f56808d51
Vladislav Hober <vladislavhober@gmail.com>
2023-08-28 22:49:20 +0300

    fixed logic of commands

--- bot-itself/bot/commands.py
+++ bot-itself/bot/commands.py
@@ -1,53 +1,88 @@
 from loggerconfig import getLogger
 
 logger = getLogger(__name__)
-
+import os
 from bot.bot import bot
 import bot.answers as answers
 from models.chat import sync_chat
-from models.client import handle_client, handle_admin, get_wait_list, admin_only, approve, approve_all
+from models.client import save, get_client_by_id, get_client_by_username, get_wait_list
 
 logger.info("Starting bot ")
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    handle_client(message)
     logger.info(f'{message.from_user.username} sent {message.text}')
+    if message.chat.type == 'private':
+        client = get_client_by_id(message.from_user.id)
+        if client is None:
+            save(message.from_user.id, message.from_user.username)
+            bot.send_message(message.chat.id, "Your request has been sent to the admin, wait for the approvement")
+        else:
+            if client.is_declined:
+                bot.send_message(message.chat.id, "Sorry, but your request was declined")
+            else:
+                if client.is_approved:
+                    bot.send_message(message.chat.id,
+                                     "Your request already has been approved. In order to get VPN credentials write "
+                                     "/get_creds")
+                else:
+                    bot.send_message(message.chat.id,
+                                     "You already requested VPN credentials. We will notify you as soon as answers "
+                                     "approves your request")
+def admin_only(func):
+    def wrapper(message):
+        client = get_client_by_id(message.from_user.id)
+        if client is None or (not client.is_admin):
+            bot.send_message(message.chat.id, "You do not have admin rights to do it!")
+            return
+        return func(message)
+    return wrapper
 
-
-
-@bot.message_handler(commands=['wait_list'])
+@bot.message_handler(commands=['waiting_list'])
 @admin_only
-def wait_list(message):
+def waiting_list(message):
+    logger.info(f'{message.from_user.username} sent {message.text}')
     clients = get_wait_list()
     clients_list = str()
     number = 1
     for client in clients:
         clients_list += f"{number}. username: @{client.username}, telegram_id: {client.tg_id}\n"
         number += 1
+    if clients_list == "":
+        clients_list = "There are no clients on the waiting list!"
     bot.send_message(message.chat.id, clients_list)
 
 @bot.message_handler(commands=['approve'])
 def client_approve(message):
-    approve(message)
+    logger.info(f'{message.from_user.username} sent {message.text}')
+    username = message.text.split(' ')[1]
+    client = get_client_by_username(username)
+    if client is None:
+        bot.send_message(message.chat.id, "This user hasn't been found!")
+        return
+    if client.is_approved:
+        bot.send_message(message.chat.id, "This user has already been approved!")
+    else:
+        client.is_approved = True
+        client.is_declined = False
+        save(client)
+        bot.send_message(message.chat.id, "This user was approved!")
 @bot.message_handler(commands=['approve_all'])
+@admin_only
 def client_approve_all(message):
-    approve_all()
+    logger.info(f'{message.from_user.username} sent {message.text}')
+    clients = get_wait_list()
+    for client in clients:
+        client.is_approved = True
+        save(client)
+    bot.send_message(message.chat.id, "All clients were approved!")
 
 @bot.message_handler(commands=['assign_admin'])
 def assign_admin(message):
+    logger.info(f'{message.from_user.username} sent {message.text}')
     callback = bot.send_message(message.chat.id, "Please enter the password to receive admin rights")
-    logger.info(f'{message.from_user.username} tried to receive admin rights')
     bot.register_next_step_handler(callback, receive_password)
-
-
-@bot.message_handler(content_types=['text'])
-def receive_password(message):
-    handle_admin(message)
-    bot.delete_message(message.chat.id, message.message_id)
-
-
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
@@ -61,3 +96,16 @@ def error(message):
     sync_chat(message)
     logger.info(f'{message.from_user.username} sent {message.text}')
     bot.reply_to(message, answers.error)
+
+@bot.message_handler(content_types=['text'])
+def receive_password(message):
+    if message.text == os.environ.get("PASSWORD"):
+        client = get_client_by_id(message.from_user.id)
+        if client is not None:
+            client.is_admin = True
+            save(client)
+            logger.info(f'{message.from_user.username} received admin rights')
+            bot.send_message(message.chat.id, "Now you're admin!")
+    else:
+        bot.send_message(message.chat.id, "Wrong password! Fuck off")
+    bot.delete_message(message.chat.id, message.message_id)
--- bot-itself/models/client.py
+++ bot-itself/models/client.py
@@ -14,18 +14,19 @@ logger.debug("Client importing.")
 class Client(base, Model):
     __tablename__ = 'client'
 
-    tg_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tg_id = Column(Integer)
     username = Column(String)
     is_approved = Column(Boolean)
     is_declined = Column(Boolean)
     is_admin = Column(Boolean)
 
-    def __init__(self, tg_id: int, username: str, is_approved: bool, is_declined: bool, is_admin: bool):
+    def __init__(self, tg_id: int, username: str):
         self.tg_id = tg_id
         self.username = username
-        self.is_approved = is_approved
-        self.is_declined = is_declined
-        self.is_admin = is_admin
+        self.is_approved = False
+        self.is_declined = False
+        self.is_admin = False
 
         logger.debug("Initializated {}".format(self.__repr__()))
 
@@ -36,77 +37,18 @@ class Client(base, Model):
         is_admin={self.is_admin}'
 
 
-def handle_client(message: telebot.types.Message):
-    if message.chat.type == 'private':
-        client = session.query(Client).filter(Client.tg_id == message.from_user.id).first()
-        if client is None:
-            client = Client(
-                message.from_user.id,
-                message.from_user.username,
-                False,
-                False,
-                False
-            )
-            client.commit()
-            bot.send_message(message.chat.id, "Your request has been sent to the supervisor, wait for the approvement")
-        else:
-            if client.is_declined:
-                bot.send_message(message.chat.id,"Your request has been sent to the supervisor, wait for the "
-                                                 "approvement")
-            else:
-                if client.is_approved:
-                    bot.send_message(message.chat.id,
-                                     "Your request already has been approved. In order to get VPN credentials write "
-                                     "/get_creds")
-                else:
-                    bot.send_message(message.chat.id,
-                                     "You already requested VPN credentials. We will notify you as soon as supervisor "
-                                     "approves your request")
+def get_client_by_id(client_id: Integer):
+    return session.query(Client).filter(Client.tg_id == client_id).first()
+def get_client_by_username(username: str):
+    return session.query(Client).filter(Client.username == username).first()
+def save(tg_id: int, username: str):
+    client = Client(tg_id, username)
+    client.commit()
 
+def save(client: Client):
+    client.commit()
 
-def handle_admin(message: telebot.types.Message):
-    if message.text == os.environ.get("PASSWORD"):
-        client = session.query(Client).filter(Client.tg_id == message.from_user.id).first()
-        if client is not None:
-            client.is_admin = True
-            client.commit()
-            logger.info(f'{message.from_user.username} received admin rights')
-            bot.send_message(message.chat.id, "Now you're admin!")
-    else:
-        bot.send_message(message.chat.id, "Wrong password! Fuck off")
-
-
-def admin_only(func):
-    def wrapper(message):
-        client = session.query(Client).filter(Client.tg_id == message.from_user.id).first()
-        if client is None or (not client.is_admin):
-            bot.send_message(message.chat.id, "You do not have admin rights to do it!")
-            return
-        return func(message)
-
-    return wrapper
-def approve(message: telebot.types.Message):
-    username = message.text.split(' ')[1]
-    client = session.query(Client).filter(Client.username == username).first()
-    if client is None:
-        bot.send_message(message.chat.id, "This user haven't been found!")
-        return
-    if client.is_approved:
-        bot.send_message(message.chat.id, "This user has already been approved!")
-    else:
-        client.is_approved = True
-        client.is_declined = False
-        client.commit()
-        bot.send_message(message.chat.id, "This user was approved!")
-
-def approve_all(message: telebot.types.Message):
-    clients = session.query(Client).filter_by(is_approved=False, is_declined=False).all()
-    for client in clients:
-        client.is_approved = True
-        client.commit()
-    bot.send_message(message.chat.id, "All clients were approved!")
 def get_wait_list():
     return session.query(Client).filter_by(is_approved=False, is_declined=False).all()
-
 
 metadata.create_all(engine)
