8660de2890a8bc95501e366d1d9981467bde320e
Valentin Kiel <val-kiel@yandex-team.ru>
2023-08-31 08:49:57 +0300

    help

--- otvali_bot/telegram_bot/commands.py
+++ otvali_bot/telegram_bot/commands.py
@@ -2,6 +2,28 @@ from .mytelebot import myTeleBot
 from models.chat import sync_chat
 from models.client import save_new_client, get_client_by_tg_id
 
+help_text = '''
+🛡️ *OtVali Help* 🛡️
+
+1. `Approval`
+   - Wait for admin approval to gain access to our servers. You will be notified once approved.
+
+2. `/server_list` 
+   - Browse through a list of available servers to find one that suits your needs.
+
+3. `/create_key <server>` e.g. /create_key Frankfurt
+   - Generate a new access key for the server specified. Use server ID, city, or country as the parameter.
+
+4. `/get_key <server>` e.g. /get_key Germany
+   - Retrieve your generated key for immediate use. The message containing the key will be deleted after 1 minute for security.
+
+5. `/my_keys`
+   - View a list of servers for which you have an access key.
+
+📌 *Note*: In future updates, you will need admin approval to access individual servers.
+'''
+
+
 def setup_default_commands(bot: myTeleBot):
     @bot.message_handler(commands=['start'])
     def send_welcome(message):
@@ -29,7 +51,7 @@ def setup_default_commands(bot: myTeleBot):
     def send_help(message):
         sync_chat(message)
         bot.logger.info(f'{message.from_user.username} sent {message.text}')
-        bot.send_message(message.chat.id, "idk sorry")
+        bot.send_message(message.chat.id, help_text)
 
 
     @bot.message_handler(func=lambda m: True)
