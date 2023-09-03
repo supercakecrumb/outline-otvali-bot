8a51a9c4bb53f03b9d0c8baf293e9148fa6d24f0
Valentin Kiel <val-kiel@yandex-team.ru>
2023-09-03 20:37:04 +0300

    hello world

--- bot-itself/models/__init__.py
+++ bot-itself/models/__init__.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 logger.debug("Creating engine.")
-engine = create_engine('sqlite:///app.db', echo=False, connect_args={'check_same_thread': False})
+engine = create_engine('sqlite:///app.db', echo=False)
 logger.debug("Creating declarative base")
 base = declarative_base(bind=engine)
 logger.debug("Creating metadata.")
--- otvali_bot/models/__init__.py
+++ otvali_bot/models/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from config.bot import sql_engine_url
 
-engine = create_engine(sql_engine_url, echo=False, connect_args={'check_same_thread': False})
+engine = create_engine(sql_engine_url, echo=False)
 base = declarative_base(bind=engine)
 metadata = base.metadata
 session = scoped_session(sessionmaker(bind=engine))
