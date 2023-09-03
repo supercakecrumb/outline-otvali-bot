6ebd1a0d25c45d8a477ef18c989925a9b032067a
Valentin Kiel <val-kiel@yandex-team.ru>
2023-09-03 21:22:40 +0300

    add docker compose up

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ set -ex
 IAM_TOKEN=$(curl -H Metadata-Flavor:Google http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token | jq -r .access_token)
 
 
-BOT_TOKEN=(curl -X GET -H "Authorization: Bearer ${IAM_TOKEN}" https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/e6qgn6al4fc7r4if5r8a/payload | jq -r '.entries[] | select(.key=="bot-token") | .textValue')
+BOT_TOKEN=$(curl -X GET -H "Authorization: Bearer ${IAM_TOKEN}" https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/e6qgn6al4fc7r4if5r8a/payload | jq -r '.entries[] | select(.key=="bot-token") | .textValue')
 ADMIN_PASSWORD=$(curl -X GET -H "Authorization: Bearer ${IAM_TOKEN}" https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/e6qgn6al4fc7r4if5r8a/payload | jq -r '.entries[] | select(.key=="admin-password") | .textValue')
+
+docker compose up
