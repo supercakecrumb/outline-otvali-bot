7bcf47ad3767460e8a8b127e3f1caf615b9bfee2
Valentin Kiel <val-kiel@yandex-team.ru>
2023-09-03 21:26:19 +0300

    -d|

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ IAM_TOKEN=$(curl -H Metadata-Flavor:Google http://169.254.169.254/computeMetadat
 BOT_TOKEN=$(curl -X GET -H "Authorization: Bearer ${IAM_TOKEN}" https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/e6qgn6al4fc7r4if5r8a/payload | jq -r '.entries[] | select(.key=="bot-token") | .textValue')
 ADMIN_PASSWORD=$(curl -X GET -H "Authorization: Bearer ${IAM_TOKEN}" https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/e6qgn6al4fc7r4if5r8a/payload | jq -r '.entries[] | select(.key=="admin-password") | .textValue')
 
-docker compose up
+docker compose up -d
